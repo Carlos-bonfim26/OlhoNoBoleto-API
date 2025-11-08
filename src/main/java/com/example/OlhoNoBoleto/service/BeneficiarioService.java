7692ae699c2f1f39433592a19cf2ebdade6b2715
carlosbonfim <carlosbonfim722@gmail.com>
2025-11-08 17:12:31 -0300

    feat:service e controller de beneficário com cadastro automático de beneficiário

--- src/main/java/com/example/OlhoNoBoleto/service/BeneficiarioService.java
+++ src/main/java/com/example/OlhoNoBoleto/service/BeneficiarioService.java
@@ -1,19 +1,38 @@
-// package com.example.OlhoNoBoleto.service;
+package com.example.OlhoNoBoleto.service;
 
-// import java.util.List;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.OlhoNoBoleto.model.Beneficiario;
+import com.example.OlhoNoBoleto.repository.BeneficiarioRepository;
 
+@Service
+public class BeneficiarioService {
 
-// import org.springframework.beans.factory.annotation.Autowired;
+    @Autowired
+    private BeneficiarioRepository beneficiarioRepository;
 
-// import com.example.OlhoNoBoleto.model.Beneficiario;
-// import com.example.OlhoNoBoleto.repository.BeneficiarioRepository;
+    public List<Beneficiario> buscarPorCnpjCpf(String cnpjCpf) {
+        return beneficiarioRepository.findByDocument(cnpjCpf);
+    }
 
-// public class BeneficiarioService {
-//     @Autowired
-//     private BeneficiarioRepository beneficiarioRepository;
-
-//     public List<Beneficiario> buscarPorCnpjCpf(String cnpjCpf) {
-//         return List.of();
-//     }
-
-// }
+    public Optional<Beneficiario> buscarOuCriarBeneficiario(String document, String nome, String banco, String agencia) {
+        Optional<Beneficiario> beneficiarioExistente = beneficiarioRepository.findByDocument(document).stream().findFirst();
+        
+        if (beneficiarioExistente.isPresent()) {
+            return beneficiarioExistente;
+        } else {
+            // Criar novo beneficiário
+            Beneficiario novoBeneficiario = new Beneficiario();
+            novoBeneficiario.setDocument(document);
+            novoBeneficiario.setNome(nome);
+            novoBeneficiario.setBanco(banco);
+            novoBeneficiario.setAgencia(agencia);
+            novoBeneficiario.setTotalQueixas(0);
+            
+            beneficiarioRepository.save(novoBeneficiario);
+            return Optional.of(novoBeneficiario);
+        }
+    }
+}
