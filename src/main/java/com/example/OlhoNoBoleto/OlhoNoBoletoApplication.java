bdc4c95f780dd284576cd52e57720ef67ad2eb1f
carlosbonfim <carlosbonfim722@gmail.com>
2025-11-19 21:54:25 -0300

    rollback: rollback na main

--- src/main/java/com/example/OlhoNoBoleto/OlhoNoBoletoApplication.java
+++ src/main/java/com/example/OlhoNoBoleto/OlhoNoBoletoApplication.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-
 @SpringBootApplication
 public class OlhoNoBoletoApplication {
 
-	public static void main(String[] args) {  
-        SpringApplication app = new SpringApplication(OlhoNoBoletoApplication.class);
-        app.setLazyInitialization(true);
-        app.setLogStartupInfo(false);
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(OlhoNoBoletoApplication.class);
+		ConfigurableApplicationContext context = app.run(args);
+
 	}
 
 }
